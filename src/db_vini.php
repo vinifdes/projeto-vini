5525ab0cef7cb81ad7f06d3f3b9b4a3fb9df6cfe
vinifdes <vinicius_fdes@outlook.com>
2024-11-21 22:06:02 -0300

    adicionando meu projeto

---
+++ src/db_vini.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'mysql-container'; // Nome do serviço no Docker Compose
+$db = 'estudos';
+$user = 'user';
+$password = 'password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    echo "Conexão com o MySQL bem-sucedida!";
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
+?>
